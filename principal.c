ba37552977c0c14f871a3b6e9200e6c3fbc1a5a2
pauyck <pauyck@gmail.com>
2017-06-23 19:56:29 -0300

    Inserindo descrição do trabalho e primeiros códigos

--- principal.c
+++ principal.c
@@ -3,6 +3,19 @@
 
 int main ()
 {
-
+	// CRIANDO PONTEIRO PARA O ARQUIVO
+	FILE *arquivoDeDados;
+	
+	// CRIANDO O ARQUIVO E APONTANDO PARA O PONTEIRO
+	arquivoDeDados = fopen(	"arquivoDeDados.txt", "w");
+	
+	// IMPRIME NO ARQUIVO
+	fprintf(arquivoDeDados, "Testes");
+	
+	//FECHA O ARQUIVO
+	fclose(arquivoDeDados);
+	
+	system("pause");
+	return 0;
 }
 
