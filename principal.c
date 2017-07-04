18f1024f87c94877586734ba45e5f836aa9870ec
pauyck <pauyck@gmail.com>
2017-07-04 00:39:46 -0300

    Corrigindo Funções

--- principal.c
+++ principal.c
@@ -3,22 +3,8 @@
 
 // VARIAVEIS GLOBAIS
 
-// ESCOPO DAS FUNÇÕES DO MENU
-void menu();
-void menuGerenciarClientes();
-void menuGerenciarProdutos();
-void menuGerenciarPedidos();
-
-// FUNÇÕES DO CLIENTE
-Cliente cadastrarCliente();
-void imprimirCliente(Cliente cliente);
-
-// FUNÇÕES DO PRODUTO
-Produto cadastrarProduto();
-void imprimirProduto (Produto produto);
-
 // CADASTRO DE CLIENTE
-// DECLARAÇÃO DA ESTRUTURA
+// DECLARA��O DA ESTRUTURA
 typedef struct Endereco {
 	char logradouro[100];
 	char cep[8];
@@ -33,7 +19,7 @@ typedef struct Cliente {
 	char cpf_cnpj[14];
 	char telefone[8];
 	Endereco enderecos[];
-} Cliente;
+}Cadcliente;
 
 
 typedef struct Produto {
@@ -43,15 +29,25 @@ typedef struct Produto {
     char categoria;
     int quantidade;
     float valorUnitario;
-} Produto;
+}cadProduto;
 
 
-Cliente cadastrarCliente();
-void imprimirCliente(Cliente cliente);
+// ESCOPO DAS FUNCOES
+void menu();
+void menuGerenciarClientes();
+void menuGerenciarProdutos();
+void menuGerenciarPedidos();
+void imprimirCliente();
+void imprimirProduto();
+void imprimirCliente();
+
+// FUN��ES COM STRUCT
+
+
 
 int main (){
 
-	// DECLARAÇÃO DAS VARIAVEIS LOCAIS
+	// DECLARA��O DAS VARIAVEIS LOCAIS
 	char opcaoMenu;
 
 
@@ -72,28 +68,28 @@ int main (){
 	// CHAMADA DA FUNÇÃO PARA IMPRIMIR O MENU
 	menu();
 
-	// LEITURA DAS OPÇÕES DO MENU
+	// LEITURA DAS OP��ES DO MENU
 
 	scanf("%c", &opcaoMenu);
 
-	// COMPARAÇÃO DAS OPÇÕES DO MENU
+	// COMPARA��O DAS OP��ES DO MENU
 	if(opcaoMenu == 'c' || opcaoMenu == 'C'){
-		// CHAMA A FUNÇÃO PARA EXIBIR O MENU CORRESPONDENTE
+		// CHAMA A FUN��O PARA EXIBIR O MENU CORRESPONDENTE
 		menuGerenciarClientes();
 	}
 	if(opcaoMenu == 'p' || opcaoMenu == 'P'){
-		// CHAMA A FUNÇÃO PARA EXIBIR O MENU CORRESPONDENTE
+		// CHAMA A FUN��O PARA EXIBIR O MENU CORRESPONDENTE
 		menuGerenciarProdutos();
 	}
 	if(opcaoMenu == 'e' || opcaoMenu == 'E'){
-		// CHAMA A FUNÇÃO PARA EXIBIR O MENU CORRESPONDENTE
+		// CHAMA A FUN��O PARA EXIBIR O MENU CORRESPONDENTE
 		menuGerenciarPedidos();
 	}
 	if(opcaoMenu == 's' || opcaoMenu == 's'){
 		return 0;
 	}
 	else{
-		printf("Opção inválida\nSaindo do sistema... \n");
+		printf("Opcao invalida\nSaindo do sistema... \n");
 	}
 
 	system("pause");
@@ -102,98 +98,97 @@ int main (){
 
 
 // FUNÇÃO MENU
-	void menu(){
-		printf("Digite um comando para prosseguir:\n");
-		printf("C - Gerenciar Clientes\n");
-		printf("P - Gerenciar Produtos\n");
-		printf("E - Gerenciar Pedidos\n");
-		printf("S - Sair\n\n");
-	}
+void menu(){
+	printf("Digite um comando para prosseguir:\n");
+	printf("C - Gerenciar Clientes\n");
+	printf("P - Gerenciar Produtos\n");
+	printf("E - Gerenciar Pedidos\n");
+	printf("S - Sair\n\n");
+}
 
 // FUNÇÃO MENU GERENCIAR CLIENTES
-	void menuGerenciarClientes(){
-		printf("============ Gerenciar Clientes ============\n");
-		printf("Digite um comando para prosseguir:\n");
-		printf("C - Cadastrar um cliente.\n");
-		printf("L - Listar todos os clientes cadastrados.\n");
-		printf("B - Buscar cliente já cadastrado.\n");
-		printf("A - Atualizar um cliente cadastrado.\n");
-		printf("E - Excluir um cliente cadastrado.\n");
-	}
+void menuGerenciarClientes(){
+	printf("============ Gerenciar Clientes ============\n");
+	printf("Digite um comando para prosseguir:\n");
+	printf("C - Cadastrar um cliente.\n");
+	printf("L - Listar todos os clientes cadastrados.\n");
+	printf("B - Buscar cliente já cadastrado.\n");
+	printf("A - Atualizar um cliente cadastrado.\n");
+	printf("E - Excluir um cliente cadastrado.\n");
+}
 
 // FUNÇÃO MENU GERENCIAR PRODUTOS
-	void menuGerenciarProdutos(){
-		printf("============== Gerenciar Produtos ==============\n");
-		printf("Digite um comando para prosseguir:\n");
-		printf("C - Cadastrar um produto.\n");
-		printf("L - Listar todos os produtos cadastrados.\n");
-		printf("B - Buscar produto já cadastrado.\n");
-		printf("A - Atualizar um produto cadastrado.\n");
-		printf("E - Excluir um produto cadastrado.\n");
-		printf("S - Atualizar estoque de um produto.\n");
-	}
+void menuGerenciarProdutos(){
+	printf("============== Gerenciar Produtos ==============\n");
+	printf("Digite um comando para prosseguir:\n");
+	printf("C - Cadastrar um produto.\n");
+	printf("L - Listar todos os produtos cadastrados.\n");
+	printf("B - Buscar produto já cadastrado.\n");
+	printf("A - Atualizar um produto cadastrado.\n");
+	printf("E - Excluir um produto cadastrado.\n");
+	printf("S - Atualizar estoque de um produto.\n");
+}
 
 // FUNÇÃO MENU GERENCIAR PEDIDOS
-	void menuGerenciarPedidos(){
-		printf("============== Gerenciar Pedidos ==============\n");
-		printf("Digite um comando para prosseguir:\n");
-		printf("C – Cadastrar um pedido.\n");
-		printf("L – Listar todos os pedidos de um cliente.\n");
-		printf("D – Listar todos os pedidos em um dia específico.\n");
-	}
+void menuGerenciarPedidos(){
+	printf("============== Gerenciar Pedidos ==============\n");
+	printf("Digite um comando para prosseguir:\n");
+	printf("C - Cadastrar um pedido.\n");
+	printf("L - Listar todos os pedidos de um cliente.\n");
+	printf("D -� Listar todos os pedidos em um dia específico.\n");
+}
 
 // FUNÇÃO CADASTRAR CLIENTE
-    Cliente cadastrarCliente() {
+cadastrarCliente() {
+    Cadcliente cliente;
+    printf("Informe o código do cliente:\n");
+    scanf(" %d", &cliente.codigo);
+    printf("Informe o nome do cliente:\n");
+    scanf(" %[^\n]s", cliente.nome);
+    printf("Informe o CPF ou CNPJ do cliente:\n");
+    scanf(" %[^\n]s", cliente.cpf_cnpj);
+    printf("Informe o telefone do cliente:\n");
+    scanf(" %[^\n]s", cliente.telefone);
 
-        Cliente cliente;
-        printf("Informe o código do cliente:\n");
-        scanf(" %d", &cliente.codigo);
-        printf("Informe o nome do cliente:\n");
-        scanf(" %[^\n]s", cliente.nome);
-        printf("Informe o CPF ou CNPJ do cliente:\n");
-        scanf(" %[^\n]s", cliente.cpf_cnpj);
-        printf("Informe o telefone do cliente:\n");
-        scanf(" %[^\n]s", cliente.telefone);
-
-        return cliente;
-}
-// FUNÇÃO IMPRIMIR CLIENTE
-    void imprimirCliente(Cliente cliente) {
-
-        printf("Código: %d\n\n", cliente.codigo);
-        printf("Nome: %s\n", cliente.nome);
-        printf("CPF: %s\n", cliente.cpf_cnpj);
-        printf("E-mail: %s\n", cliente.telefone);
-        printf("Endereço: %s\n", cliente.enderecos);
+    //return cliente;
 }
 
 
-// FUNÇÃO CADASTRAR PRODUTO
-    Produto cadastrarProduto() {
-
-        Produto produto;
-        printf("Informe o código do produto:\n");
-        scanf(" %d", &produto.codigo);
-        printf("Informe o nome do produto:\n");
-        scanf(" %[^\n]s", produto.nome);
-        printf("Informe o fornecedor:\n");
-        scanf(" %[^\n]s", produto.fornecedor);
-        printf("Informe a quantidade:\n");
-        scanf(" %d", produto.quantidade);
-        printf("Informe o valor unitário do produto:\n");
-        scanf(" %f", produto.valorUnitario);
-
-        return produto;
-    }
-
-//  FUNÇÃO IMPRIMIR PRODUTO
-    void imprimirProduto(Produto Produto) {
-
-        printf("Código: %d\n\n", Produto.codigo);
-        printf("Nome: %s\n", produto.nome);
-        printf("Fornecedor: %s\n", produto.fornecedor);
-        printf("Quantidade; %d\n", produto.quantidade);
-        printf("Valor Unitário: %.f\n", produto.valorUnitario);
+// FUN��O IMPRIMIR CLIENTE
+void imprimirCliente(Cadcliente x) {
+	Cadcliente cliente;
+    printf("Codigo: %d\n\n", cliente.codigo);
+    printf("Nome: %s\n", cliente.nome);
+    printf("CPF: %s\n", cliente.cpf_cnpj);
+    printf("E-mail: %s\n", cliente.telefone);
+    printf("Endereco: %s\n", cliente.enderecos);
 }
-    }
+
+
+// FUN��O CADASTRAR PRODUTO
+cadProduto cadastrarProduto() {
+    cadProduto produto;
+    printf("Informe o codigo do produto:\n");
+    scanf(" %d", &produto.codigo);
+    printf("Informe o nome do produto:\n");
+    scanf(" %[^\n]s", produto.nome);
+    printf("Informe o fornecedor:\n");
+    scanf(" %[^\n]s", produto.fornecedor);
+    printf("Informe a quantidade:\n");
+    scanf(" %d", produto.quantidade);
+    printf("Informe o valor unitario do produto:\n");
+    scanf(" %f", produto.valorUnitario);
+
+    return produto;
+}
+
+//  FUN��O IMPRIMIR PRODUTO
+    void imprimirProduto(cadProduto Produto) {
+		cadProduto imprimeprod;
+        printf("Codigo: %d\n\n", imprimeprod.codigo);
+        printf("Nome: %s\n", imprimeprod.nome);
+        printf("Fornecedor: %s\n", imprimeprod.fornecedor);
+        printf("Quantidade; %d\n", imprimeprod.quantidade);
+        printf("Valor Unitario: %.f\n", imprimeprod.valorUnitario);
+}
 
